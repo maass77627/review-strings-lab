afd26073d2d179fb5f44f6f0e04945db513cb9a0
kelly_maass <kmaass77627@gmail.com>
2025-11-07 12:39:19 -0600

    done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
+
+
 const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = 'Welcome, ' + currentUser[0]+'!';  
-
-
 
 
 
